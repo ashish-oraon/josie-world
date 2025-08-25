4f3be1e71e4118c0748aec7cac985c2657519fc3
Ashish Oraon <ashish.oraon@ect-telecoms.de>
2025-08-25 11:13:29 +0200

    Major Update with transaction and reports

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JOJLUS2L.mjs";import"./chunk-VSNUKKYV.mjs";import"./chunk-BYBRTMLD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-VSC3ZDWU.mjs";import"./chunk-ZOGYC3YL.mjs";import"./chunk-BYBRTMLD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

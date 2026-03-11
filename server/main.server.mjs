6da847a0f31e504f39c88ac19d592c4194293e60
Ashish Oraon <ashish.oraon@ect-telecoms.de>
2026-03-11 15:19:22 +0100

    style changes

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XTXAJTYC.mjs";import"./chunk-EGZBDO3B.mjs";import"./chunk-NGUIIWC3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ZH723LXK.mjs";import"./chunk-EGZBDO3B.mjs";import"./chunk-NGUIIWC3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
